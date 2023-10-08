dd09272d6deb05cefef69f3bd5c60d0ba4fb06ce
Michael Johne <mjohne@users.noreply.github.com>
2023-10-08 22:50:00 +0200

    Add files via upload

---
+++ AboutBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+using MijoSoftware.AssemblyInformation;
+
+namespace HashLister
+{
+	/// <summary>
+	/// AboutBox
+	/// </summary>
+	internal partial class AboutBox : Form
+	{
+		public AboutBox() => InitializeComponent();
+
+		private void AboutBox_Load(object sender, System.EventArgs e)
+		{
+			Text = $"Info über {AssemblyInfo.AssemblyTitle}";
+			labelProductName.Text = AssemblyInfo.AssemblyProduct;
+			labelVersion.Text = $"Version {AssemblyInfo.AssemblyVersion}";
+			labelCopyright.Text = AssemblyInfo.AssemblyCopyright;
+			labelCompanyName.Text = AssemblyInfo.AssemblyCompany;
+			textBoxDescription.Text = AssemblyInfo.AssemblyDescription;
+		}
+	}
+}
